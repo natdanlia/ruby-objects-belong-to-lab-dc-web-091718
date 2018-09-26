cbd1303c2548a102ece1c65f1621d37e967ec6ce
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-09-26 09:56:31 -0400

    Done

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+  attr_accessor :name
+
+
+
+end
+# teddy = Artist.new
+#
+# teddy.name = "teddy afro"
+#
+# abeba = Song.new
+#
+# abeba.title = "abebayohosh"
---
+++ lib/song.rb
@@ -0,0 +1,6 @@
+# require "pry"
+class Song
+  attr_accessor :title, :artist
+  # binding.pry
+
+end
